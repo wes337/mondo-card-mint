7ac30acb76518ec8cd11d0d007d5418d75ab0f89
wes337 <wesmoses@gmail.com>
2022-09-01 21:59:36 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import { Component, onMount, onCleanup, createSignal } from "solid-js";
+import "./App.scss";
+import { mintCard, clearMintedCards } from "./Minter";
+import { getCardImage, getRandomCards } from "./utils";
+
+const App: Component = () => {
+  const [mintedCards, setMintedCards] = createSignal([]);
+
+  onMount(() => {
+    clearMintedCards();
+  });
+
+  onCleanup(() => {
+    clearMintedCards();
+  });
+
+  const onClickCard = (event) => {
+    const card = event.target;
+    const callback = (cardRef) => {
+      const card = JSON.parse(cardRef.getAttribute("data-card"));
+      setMintedCards([...new Set([...mintedCards(), card])]);
+    };
+
+    const random = Math.random() < 0.5;
+
+    mintCard([card], {
+      callback,
+      fallToLeft: random,
+    });
+  };
+
+  const clear = () => {
+    setMintedCards([]);
+    clearMintedCards();
+  };
+
+  return (
+    <div class="app">
+      <div class="card-select">
+        <h2>
+          Click on a card to <span class="glow">MINT</span> it
+        </h2>
+        <div class="card-select-cards">
+          {getRandomCards(5).map((randomCard) => (
+            <img
+              src={getCardImage(randomCard)}
+              width={168}
+              height={284}
+              alt=""
+              data-card={JSON.stringify(randomCard)}
+              onClick={onClickCard}
+            />
+          ))}
+        </div>
+      </div>
+
+      {mintedCards().length && (
+        <div class="minted-card-display" onClick={clear}>
+          {mintedCards().map((card) => {
+            const src = getCardImage(card);
+            return <img src={src} alt="" />;
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Minter.ts
@@ -0,0 +1,101 @@
+interface mintCardOptions {
+  g?: number;
+  dt?: number;
+  bounce?: number;
+  endVelocity?: number;
+  stagger?: number;
+  fallToLeft?: boolean;
+  clear?: boolean;
+  callback?: (card: any) => void;
+}
+
+const MINT_CARD_ANIMATION_CLASS = "minted-card-animation";
+
+export const mintCard = (cardRefs, options?: mintCardOptions) => {
+  const g = options?.g || -3;
+  const dt = options?.dt || 5;
+  const bounce = options?.bounce || 0.99;
+  const endVelocity = options?.endVelocity || 0;
+  const stagger = options?.stagger || 200;
+  const fallToLeft = options?.fallToLeft || false;
+  const clear = options?.clear || false;
+  const callback = options?.callback || null;
+
+  const windowHeight = window.innerHeight;
+
+  const fallIteration = (elem, elemHeight, oldPos, dx, dy) => {
+    const copy = elem.cloneNode(true);
+    document.body.append(copy);
+
+    const newTop = Math.min(windowHeight - elemHeight, oldPos.top + dy);
+    const newPos = {
+      left: oldPos.left + dx,
+      top: newTop,
+    };
+
+    copy.style.top = `${newPos.top}px`;
+    copy.style.left = `${newPos.left}px`;
+
+    if (Math.abs(newTop - (windowHeight - elemHeight)) < 5) {
+      if (newPos.left > window.innerWidth || newPos.left < 0) {
+        callback?.(elem);
+      } else if (dy < 0 || dy > endVelocity) {
+        dy *= -1 * bounce;
+        setTimeout(() => {
+          fallIteration(copy, elemHeight, newPos, dx, dy);
+        }, dt);
+      } else {
+        // Animation finished
+        callback?.(elem);
+      }
+    } else {
+      dy = dy - g;
+      setTimeout(() => {
+        fallIteration(copy, elemHeight, newPos, dx, dy);
+      }, dt);
+    }
+  };
+
+  const startFall = (elem, height, stagger) => {
+    let dx = Math.floor(Math.random() * 10) + 5;
+
+    if (fallToLeft) {
+      dx = -dx;
+    }
+
+    const copy = elem.cloneNode(true);
+    copy.classList.add(MINT_CARD_ANIMATION_CLASS);
+    copy.style.position = "fixed";
+    copy.style.top = `${elem.offsetTop}px`;
+    copy.style.left = `${elem.offsetLeft}px`;
+    copy.style.zIndex = 1;
+    document.body.append(copy);
+
+    setTimeout(() => {
+      const position = {
+        top: copy.offsetTop,
+        left: copy.offsetLeft,
+      };
+      fallIteration(copy, height, position, dx, 0);
+    }, stagger);
+  };
+
+  if (clear) {
+    clearMintedCards();
+  }
+
+  cardRefs.forEach((cardRef) => {
+    if (
+      cardRef.offsetHeight < window.outerHeight &&
+      !cardRef.classList.contains(`.${MINT_CARD_ANIMATION_CLASS}`)
+    ) {
+      startFall(cardRef, cardRef.offsetHeight, 1 * stagger);
+    }
+  });
+};
+
+export const clearMintedCards = () => {
+  document
+    .querySelectorAll(`.${MINT_CARD_ANIMATION_CLASS}`)
+    .forEach((element) => element.remove());
+};
